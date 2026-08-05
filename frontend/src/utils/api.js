e303d94525c8ecc10e409dd1d2bdf0dc94fbf4af
ruksanakasim <anziballianze@gmail.com>
2026-08-05 11:31:08 +0530

    api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,21 @@
 import axios from 'axios'
 
+let baseUrl;
+
+const ENV = import.meta.env.VITE_ENV;
+
+// console.log(ENV);
+if (ENV === "development") {
+  baseUrl = "http://localhost:7000/";
+} else if (ENV === "production") {
+  baseUrl = "https://www.test.camet.in";
+} 
+
+console.log(Base URL: ${baseUrl});
+
+
 const api = axios.create({
-  // baseURL: 'http://localhost:7000/api'
-   baseURL: 'https://www.test.camet.in'
-})
+  baseURL: baseUrl
+});
 
 export default api
